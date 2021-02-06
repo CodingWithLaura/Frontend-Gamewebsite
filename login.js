5bd777510fd48473ec8babc149be043f858dd0bc
Major&Hannah <majorx234@googlemail.com>
2021-02-06 20:50:02 +0100

    added onclick functionality to a div-tag

--- login.js
+++ login.js
@@ -1,3 +1,8 @@
 function submit(){
-    console.log('Hello!');
+    var form_div = document.getElementById('login_form');
+    var inputs   = form_div.getElementsByTagName("input");
+    var username = inputs[0].value;
+    var password = inputs[1].value;
+    var ausgabe = "hallo " + username;
+    console.log(ausgabe);
 }
